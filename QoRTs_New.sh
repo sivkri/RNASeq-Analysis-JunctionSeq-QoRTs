8fe99db59bccd0a26588a91eae47226cb8e1da34
Sivakumar Krishnamoorthy <indiangene@gmail.com>
2023-06-07 17:00:33 +0200

    Update QoRTs_New.sh

--- QoRTs_New.sh
+++ QoRTs_New.sh
@@ -1,9 +1,19 @@
 #!/bin/bash -l
-#SBATCH -J QoRts_new
+#SBATCH -J QoRTs_new
 #SBATCH -N 1 
 #SBATCH --ntasks-per-node 4
 #SBATCH --mem 4gb
 #SBATCH --time=02:00:00
 #SBATCH --mail-type=ALL 
 
-java -Xmx4G -jar /QoRTs-master/QoRTs.jar QC \--stranded \--runFunctions writeKnownSplices,writeNovelSplices,writeSpliceExon \/Controlrep1Aligned.sortedByCoord.out.bam \Arabidopsis_thaliana.TAIR10.42.gtf \QoRTs_out_new/Controlrep1/
+QORTS_JAR="/path/to/QoRTs.jar"
+INPUT_BAM="/path/to/Controlrep1Aligned.sortedByCoord.out.bam"
+ANNOTATION_GTF="/path/to/Arabidopsis_thaliana.TAIR10.42.gtf"
+OUTPUT_DIR="QoRTs_out_new/Controlrep1/"
+
+java -Xmx4G -jar /QoRTs-master/QoRTs.jar QC \
+  --stranded \
+  --runFunctions writeKnownSplices,writeNovelSplices,writeSpliceExon \
+  --inputBam "$INPUT_BAM" \
+  --annotationGTF "$ANNOTATION_GTF" \
+  --outputDir "$OUTPUT_DIR"
