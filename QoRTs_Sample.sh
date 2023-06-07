052ad523b7329fb5809e917ef718919c6d197edb
Sivakumar Krishnamoorthy <indiangene@gmail.com>
2023-06-07 17:01:17 +0200

    Update QoRTs_Sample.sh

--- QoRTs_Sample.sh
+++ QoRTs_Sample.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node 4
 #SBATCH --mem 4gb
 #SBATCH --time=01:00:00
-#SBATCH --mail-type=ALL
+#SBATCH --mail-type=ALL 
 
 export TMPDIR="/tmp/lustre_shared/${USER}/${SLURM_JOBID}"
 export SCR=${TMPDIR}
@@ -12,13 +12,12 @@ mkdir -p ${TMPDIR}
 cp ${SLURM_SUBMIT_DIR}/${INPUT_DIR}/* ${TMPDIR}
 cd $TMPDIR
 
-java -Xmx4G -jar /QoRTs-master/QoRTs.jar QC \ 
+java -Xmx4G -jar /QoRTs-master/QoRTs.jar QC \
 	--stranded --minMAPQ 50 \
 	--runFunctions writeKnownSplices,writeNovelSplices,writeSpliceExon \
 	/ABI1rep1Aligned.sortedByCoord.out.bam \
-	transcriptome.gtf \ 
+	transcriptome.gtf \
 	QoRTs_out/1/
 
 mkdir $SLURM_SUBMIT_DIR/${OUTPUT_DIR}
 cp -r $TMPDIR/* $SLURM_SUBMIT_DIR/${OUTPUT_DIR}/
-
